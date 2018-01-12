b5dcd05cea03873081efd2c148ece4b3fe506e17
Thomas Gelf <thomas@gelf.net>
2018-01-12 15:15:47 +0100

    LegacyCommandPipe: allows to run on legacy...
    ...systems without a fully configured monitoring module

---
+++ library/Jira/LegacyCommandPipe.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Icinga\Module\Jira;
+
+use Icinga\Exception\IcingaException;
+
+class LegacyCommandPipe
+{
+    protected $pipe;
+
+    public function __construct($pipe)
+    {
+        $this->pipe = $pipe;
+    }
+
+    public function acknowledge($author, $message, $host, $service)
+    {
+        $pipe = $this->pipe;
+        if (@file_exists($pipe)) {
+            if (@is_writable($pipe)) {
+                $result = file_put_contents($pipe, $this->renderAck($author, $message, $host, $service));
+
+                return $result !== false;
+            } else {
+                throw new IcingaException(
+                    '%s is not writable, cannot send %s',
+                    $pipe,
+                    $this->shorten($message, 60)
+                );
+            }
+        } else {
+            throw new IcingaException(
+                '%s does not exist, cannot send %s',
+                 $pipe,
+                 $this->shorten($message, 60)
+             );
+        }
+    }
+
+    protected function shorten($string, $maxLength)
+    {
+        if (strlen($string) > $maxLength) {
+            return substr($string, 0, $maxLength) . '...';
+        } else {
+            return $string;
+        }
+    }
+
+    protected function renderAck(
+        $author,
+        $comment,
+        $host,
+        $service = null,
+        $notify = true,
+        $sticky = false,
+        $persistent = false,
+        $expireTime = null
+    ) {
+        if ($expireTime === null) {
+            $expireSuffix = '';
+        } else {
+            $expireSuffix = '_EXPIRE';
+        }
+        if ($service === null) {
+            $cmd = "ACKNOWLEDGE_HOST_PROBLEM$expireSuffix;$host";
+        } else {
+            $cmd = "ACKNOWLEDGE_SVC_PROBLEM$expireSuffix;$host;$service";
+        }
+
+
+        $cmd .= sprintf(
+            ';%u;%u;%u',
+            $sticky ? '2' : '0',
+            (string) $notify,
+            $persistent
+        );
+        if ($expireTime !== null) {
+            $cmd .= ";$expireTime";
+        }
+
+        return $cmd . ";$author;$comment";
+    }
+}
